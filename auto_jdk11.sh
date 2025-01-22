07d09135401c68d9b8dd666f06c1f40ce42d286e
guohaolu <110468853+guohaolu@users.noreply.github.com>
2025-01-22 15:35:26 +0800

    Update auto_jdk11.sh

--- auto_jdk11.sh
+++ auto_jdk11.sh
@@ -5,11 +5,11 @@ wget https://repo.huaweicloud.com/openjdk/11.0.1/openjdk-11.0.1_linux-x64_bin.ta
 tar -zxvf openjdk-11.0.1_linux-x64_bin.tar.gz
 mv jdk-11.0.1/ /usr/local/
 
-echo " " >> /etc/profile
-echo "# jdk11" >> /etc/profile
-echo "export JAVA_HOME=/usr/local/jdk-11.0.1" >> /etc/profile
-echo "export CLASSPATH=$:CLASSPATH:$JAVA_HOME/lib/" >> /etc/profile
-echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile
+echo '' | sudo tee -a /etc/profile
+echo '# jdk11' | sudo tee -a /etc/profile
+echo 'export JAVA_HOME=/usr/local/jdk-11.0.1' | sudo tee -a /etc/profile
+echo 'export CLASSPATH=.:$CLASSPATH:$JAVA_HOME/lib/' | sudo tee -a /etc/profile
+echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile
 
 source /etc/profile
 java -version
