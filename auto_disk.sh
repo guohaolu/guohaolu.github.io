bee00fa65346cb2d1e9d07161ec7a975c926b5bf
guohaolu <110468853+guohaolu@users.noreply.github.com>
2025-01-22 11:04:13 +0800

    Update auto_disk.sh

--- auto_disk.sh
+++ auto_disk.sh
@@ -16,11 +16,11 @@ w
 EOF
 
 echo -e "\n\n**************** 格式化磁盘 ****************\n\n"
-sleep 5s
+sleep 30s
 
 mkfs.ext4 /dev/vdb1
 
 echo -e "\n\n**************** mount挂载磁盘 ****************\n\n"
-sleep 1s
+sleep 20s
 
 echo "/dev/vdb1 /data/data1 ext4 defaults 0 0" >> /etc/fstab
