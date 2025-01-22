c5723c7411c55c3111d9ec3d246ac0c64447c10a
guohaolu <110468853+guohaolu@users.noreply.github.com>
2025-01-22 16:38:22 +0800

    Update auto_jdk8.sh

--- auto_jdk8.sh
+++ auto_jdk8.sh
@@ -7,7 +7,7 @@ mv dragonwell-8.23.22/ /usr/local/
 
 echo '' | sudo tee -a /etc/profile
 echo '# jdk8' | sudo tee -a /etc/profile
-echo 'export JAVA_HOME=/usr/local/jdk-11.0.1' | sudo tee -a /etc/profile
+echo 'export JAVA_HOME=/usr/local/dragonwell-8.23.22' | sudo tee -a /etc/profile
 echo 'export CLASSPATH=.:$CLASSPATH:$JAVA_HOME/lib/' | sudo tee -a /etc/profile
 echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile
 
